817dbb85ba92b88ecfc18b7a2dfba0a058d7b8c1
MasabRehman <masabrehman@example.com>
2026-07-10 02:19:14 +0500

    Remove incompatible SRI plugin to fix Vercel build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sri from '@small-tech/vite-plugin-sri';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sri()],
+  plugins: [react()],
   server: {
     port: 3000,
     proxy: {
